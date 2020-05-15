0338da8ceefc01f754eea62d8b511d22609aa1be
CoreyDeJong <dejongcorey@gmail.com>
2020-05-15 00:11:42 -0700

    code 4 complete

--- challenges/matrix_array/tests/test_matrix_array.py
+++ challenges/matrix_array/tests/test_matrix_array.py
@@ -1,5 +1,12 @@
-from matrix_array import __version__
+from matrix_array.matrix_array import matrix_arr
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_matrix1():
+    actual = matrix_arr([[1,2,3],[3,5,7],[1,7,10]])
+    expected = [6,15,18]
+    assert actual == expected
+
+def test_matrix2():
+    actual = matrix_arr([[0,1,5],[-4,7,2],[-3,12,11]])
+    expected = [6,5,20]
+    assert actual == expected
