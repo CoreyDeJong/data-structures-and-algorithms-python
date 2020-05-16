c0513c863cd5db1c255969cc41890a5e5ad92973
CoreyDeJong <dejongcorey@gmail.com>
2020-05-16 13:46:38 -0700

    some tests are created:

--- challenges/data_structures/linked_list/tests/test_linked_list.py
+++ challenges/data_structures/linked_list/tests/test_linked_list.py
@@ -1,5 +1,47 @@
-from linked_list import __version__
+import pytest
+from linked_list.linked_list import LinkedList, Node
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_instance():
+    ll = LinkedList()
+    assert ll.head == None
+
+
+"""
+Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
+Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
+"{ a } -> { b } -> { c } -> NULL"
+"""
+
+
+def test_insert_empty():
+    ll = LinkedList()
+    ll.insert("apples")
+    assert ll.head.value == "apples"
+
+
+def test_insert_full():
+    ll = LinkedList()
+    ll.insert("apples")
+    ll.insert("bananas")
+    assert ll.head.value == "bananas"
+    assert ll.head.next.value == "apples"
+
+
+
+
+def test_linked_list_repr():
+    ll = LinkedList()
+    actual = repr(ll)
+    expected = "LinkedList : None"
+    assert actual == expected
+
+def test_linked_list_str():
+    ll = LinkedList()
+    ll.insert("apples")
+    ll.insert("bananas")
+    assert str(ll) == "{ bananas } -> { apples } -> NULL"
+
+def test_node_exception():
+    with pytest.raises(TypeError):
+        Node("sample", "this is NOT a Node")
