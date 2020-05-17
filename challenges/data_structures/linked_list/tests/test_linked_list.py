868c43c69f6a01f5a5a518d48b19c298e0a2701d
CoreyDeJong <dejongcorey@gmail.com>
2020-05-16 19:58:02 -0700

    assignment complete

--- challenges/data_structures/linked_list/tests/test_linked_list.py
+++ challenges/data_structures/linked_list/tests/test_linked_list.py
@@ -1,25 +1,38 @@
 import pytest
 from linked_list.linked_list import LinkedList, Node
 
-
+## linkedlist head
 def test_instance():
     ll = LinkedList()
     assert ll.head == None
 
+## linkedlist repr
+def test_linked_list_repr():
+    ll = LinkedList()
+    actual = repr(ll)
+    expected = "LinkedList : None"
+    assert actual == expected
 
-"""
-Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
-Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
-"{ a } -> { b } -> { c } -> NULL"
-"""
 
 
+#### Insert ######
+
+## linkedlist str
+# Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
+# "{ a } -> { b } -> { c } -> NULL"
+def test_linked_list_str():
+    ll = LinkedList()
+    ll.insert("apples")
+    ll.insert("bananas")
+    assert str(ll) == "{ bananas } -> { apples } -> NULL"
+
+## linkedlist insert
 def test_insert_empty():
     ll = LinkedList()
     ll.insert("apples")
     assert ll.head.value == "apples"
 
-
+## linkedlist
 def test_insert_full():
     ll = LinkedList()
     ll.insert("apples")
@@ -28,20 +41,41 @@ def test_insert_full():
     assert ll.head.next.value == "apples"
 
 
+#### Includes ####
+# Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
 
-
-def test_linked_list_repr():
-    ll = LinkedList()
-    actual = repr(ll)
-    expected = "LinkedList : None"
-    assert actual == expected
-
-def test_linked_list_str():
+def test_includes_true():
     ll = LinkedList()
     ll.insert("apples")
     ll.insert("bananas")
-    assert str(ll) == "{ bananas } -> { apples } -> NULL"
+    ll.insert("tomato")
+    actual = ll.includes("apples")
+    expected = True
+    assert actual == expected
+
+def test_includes_false():
+    ll = LinkedList()
+    ll.insert("apples")
+    ll.insert("bananas")
+    ll.insert("tomato")
+    actual = ll.includes('pineapple')
+    expected = False
+    assert actual == expected
+
+
+
+### Error Test ######
 
 def test_node_exception():
     with pytest.raises(TypeError):
         Node("sample", "this is NOT a Node")
+
+# test to see if there is a node connected to lucy
+# isinstance(lucy.next, node)
+
+@pytest.fixture()
+def prep_fruit():
+    ll = LinkedList()
+    ll.insert("apples")
+    ll.insert("bananas")
+    ll.insert("bananas2")
