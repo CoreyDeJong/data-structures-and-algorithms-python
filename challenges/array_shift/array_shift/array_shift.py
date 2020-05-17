052d2a448a89d2583eae4adb2f85e03da527ca5d
CoreyDeJong <dejongcorey@gmail.com>
2020-05-16 23:57:28 -0700

    updating files

---
+++ challenges/array_shift/array_shift/array_shift.py
@@ -0,0 +1,16 @@
+
+import math
+def insertShiftArray(array, new_number):
+    middle = (len(array)//2)-1
+    
+    
+    answer = []
+
+    for i in range(len(array)):
+        answer.append(array[i])
+        if i == middle:
+            answer.append(new_number)
+        i+=1
+
+    return answer
+
