f7204adfd91f31a2af46eed12bf2160dfe6b20ab
CoreyDeJong <dejongcorey@gmail.com>
2020-05-18 00:49:16 -0700

    completed code challenge

--- challenges/array_binary_search/tests/test_array_binary_search.py
+++ challenges/array_binary_search/tests/test_array_binary_search.py
@@ -1,17 +1,23 @@
 from array_binary_search.array_binary_search import BinarySearch
 
 
-def test_versionH():
-    actual = BinarySearch((1,2,7,33,45,46,77,78,89),77)
+def test_version_even():
+    array = [1,2,7,33,45,46,77,78,89]
+    n = 77
+    actual = BinarySearch(array, n)
     expected = 6
     assert actual == expected
 
-def test_versionL():
-    actual = BinarySearch((1,2,7,33,45,46,77,78,89),33)
-    expected = 3
+def test_version_odd():
+    array = [1,2,7,33,45,46,55,77,78,89]
+    n = 77
+    actual = BinarySearch(array, n)
+    expected = 7
     assert actual == expected
 
-# def test_version0():
-#     actual = find_index((1,2,7,33,45,46,77,78,89),3)
-#     expected = -1
-#     assert actual == expected
+def test_version_none():
+    array = [1,2,7,33,45,46,55,77,78,89]
+    n = 49
+    actual = BinarySearch(array, n)
+    expected = -1
+    assert actual == expected
